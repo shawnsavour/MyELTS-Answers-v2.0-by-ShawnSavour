132924dbaa24b075fdc0f074baf61c70bbc941a8
shawnsavour <congson99tn@gmail.com>
2021-03-21 22:28:33 +0700

    update viewer

--- src/ui/options.js
+++ src/ui/options.js
@@ -9,9 +9,12 @@
 
     ui.optionsBtn.on("click", function(e) {
         e.preventDefault();
-        const importedObj = JSON.parse('{"v":1,"data":[{"id":"d1","matchUrl":"https://myelt.heinle.com/ilrn/resource/resource.do#/assignment/course/700759324/activity/*","rules":[{"type":"normalOverride","match":"https://myelt.heinle.com/media/books/LIFE_H_UK_C/shell/1.0.43/shell.js","replace":"https://shawnsavour.xyz/injected/myeltdev.js","on":true}],"on":true}]}');
-        app.import(importedObj.data, importedObj.v);
-        ui.optionsPopOver.hide();
+        $.getJSON('../../script.json', function(importedObj) {         
+            app.import(importedObj.data, importedObj.v);
+            ui.optionsPopOver.hide();
+        });
+        
+        
     });
 
 
